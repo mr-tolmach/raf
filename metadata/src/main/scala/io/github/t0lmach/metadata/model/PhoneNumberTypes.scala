23c0dad634d97a5bc9075a98b995888495385aea
Daniil Tolmachev <mister.tolmach@gmail.com>
2022-12-27 08:10:22 +0000

    Support libphonenumber 8.12.57

--- metadata/src/main/scala/io/github/t0lmach/metadata/model/PhoneNumberTypes.scala
+++ metadata/src/main/scala/io/github/t0lmach/metadata/model/PhoneNumberTypes.scala
@@ -4,7 +4,17 @@ object PhoneNumberTypes extends Enumeration {
 
   type PhoneNumberType = Value
 
-  val FixedLine, FixedLineOrMobile, Mobile, Pager, PersonalNumber, PremiumRate, SharedCost, TollFree, UAN, VoIP, Voicemail = Value
+  val FixedLine = Value
+  val FixedLineOrMobile = Value
+  val Mobile = Value
+  val Pager = Value
+  val PersonalNumber = Value
+  val PremiumRate = Value
+  val SharedCost = Value
+  val TollFree = Value
+  val UAN = Value
+  val VoIP = Value
+  val Voicemail = Value
 
   val All: Set[PhoneNumberType] = values
 
